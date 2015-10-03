d2fce7518ffba451ecd185072c074639deae4ccf
Gali Chen <w2812451@gmail.com>
2015-10-03 21:41:46 +0800

    First implement for lab-1 assembly fibonacci function
    The modified "lab-1/fib.s" file is an assembly source code, which
    include a recursive fibonacci funciton.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -9,35 +9,22 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, lr}		@ PROLOG
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs r4, r0, #1			@ R4 = R0 - 1 (update flags)
+	ble .end			@ if ( R0 <= 1 ) goto .end ( which returns R0 )
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	mov r0, r4			@ R0 = R4
+	bl fibonacci			@ Recursive call to fibonacci with `R0 - 1 as parameter
+	mov r5, r0			@ Keep fibonacci(R0 - 1) result at R3
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	sub r0, r4, #1			@ R0 = R4 - 1
+	bl fibonacci			@ Recursive call to fibonacci with `R0 - 2 as parameter
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	add r0, r5, r0			@ R0 = R3 + R0
 
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+.end:
+	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
